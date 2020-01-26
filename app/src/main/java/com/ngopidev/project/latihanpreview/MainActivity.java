e6efaffe31015b87e37f890f7e47e7d4d475acdd
Irfan <assidiq.irfan@gmail.com>
2020-01-26 09:38:46 +0700

    android complete list

--- app/src/main/java/com/ngopidev/project/latihanpreview/MainActivity.java
+++ app/src/main/java/com/ngopidev/project/latihanpreview/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn2, btn3;
+    Button btn2, btn3, btn5, btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,30 +18,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
+        //baru ditambahkan :
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
 
         //anonymous inner class
-        btn2.setOnClickListener(new View.OnClickListener() {
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(MainActivity.this,
+//                        ListViewDenganAdapter.class);
+//                startActivity(i);
+//            }
+//        });
+
+        View.OnClickListener tombolKlik = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,
+                switch (view.getId()){
+                    case R.id.btn2 : {
+                        Intent i = new Intent(MainActivity.this,
                         ListViewDenganAdapter.class);
-                startActivity(i);
+                        startActivity(i);
+                    }break;
+                    case R.id.btn5 : {
+                        Toast.makeText(MainActivity.this, "button 5 terklik",
+                                Toast.LENGTH_SHORT).show();
+                    }break;
+                    default:{
+                        Toast.makeText(MainActivity.this, "button tidak ada",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
-        });
+        };
+
+        btn2.setOnClickListener(tombolKlik);
+        btn5.setOnClickListener(tombolKlik);
+
 
         //menggunakan implement onclikclistener
         btn3.setOnClickListener(this);
+        btn6.setOnClickListener(this);
     }
 
     //digunakan saat menyimpan onClick di xmlnya
     public void klikbutton1(View v){
-        Intent i = new Intent(this, ListViewTanpaAdapter.class);
-        startActivity(i);
+      if(v.getId() == R.id.btn1){
+          Intent i = new Intent(this, ListViewTanpaAdapter.class);
+          startActivity(i);
+      }else if(v.getId() == R.id.btn4){
+          Toast.makeText(this, "Button 4 ter klik", Toast.LENGTH_SHORT).show();
+      }else{
+          Toast.makeText(this, "button tidak ada", Toast.LENGTH_SHORT).show();
+      }
+
     }
 
     @Override
     public void onClick(View view) {
-        Intent i = new Intent(MainActivity.this, LayoutCampuran.class);
-        startActivity(i);
+       if(view.getId() == R.id.btn3){
+           Intent i = new Intent(MainActivity.this, LayoutCampuran.class);
+           startActivity(i);
+       }else if(view.getId() == R.id.btn6){
+           Toast.makeText(this, "button 6 ter klik",
+                   Toast.LENGTH_SHORT).show();
+       }else {
+           Toast.makeText(this, "button tidak ada",
+                   Toast.LENGTH_SHORT).show();
+       }
     }
 }
